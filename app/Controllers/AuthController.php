bb38cf2eb13ae6fe905d55256a20b2ec9a7c907b
Idham <idhamadileksono@gmail.com>
2024-07-01 20:00:16 +0700

    add a git

---
+++ app/Controllers/AuthController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\UserModel;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class AuthController extends BaseController
+{
+    protected $user;
+    public function index()
+    {
+    }
+    function __construct()
+    {
+        helper('form');
+        $this->user = new UserModel();
+    }
+
+    public function login()
+    {
+        if ($this->request->getPost()) {
+            $rules = [
+                'username' => 'required|min_length[6]',
+                'password' => 'required|min_length[5]numeric',
+            ];
+
+            if ($this->validate($rules)) {
+                $username = $this->request->getVar(('username'));
+                $password = $this->request->getVar(('password'));
+
+                // $username = $this->request->getVar('username');
+                // $password = $this->request->getVar('password');
+
+                $dataUser = $this->user->where(['username' => $username])->first(); // pass 12345
+
+                if ($dataUser) {
+                    if (password_verify($password, $dataUser['password'])) {
+                        session()->set([
+                            'username' => $dataUser['username'],
+                            'role' => $dataUser['role'],
+                            'isLoggedIn' => TRUE
+                        ]);
+
+                        return redirect()->to(base_url('home'));
+                    } else {
+                        session()->setFlashdata('failed', 'Username & Password Salah');
+                        return redirect()->back();
+                    }
+                } else {
+                    session()->setFlashdata('failed', 'Username Tidak Ditemukan');
+                    return redirect()->back();
+                }
+            } else {
+                session()->setFlashdata('failed', $this->validator->listErrors());
+                return redirect()->back();
+            }
+        }
+        return view('v_login');
+    }
+
+    public function logout()
+    {
+        session()->destroy();
+        return redirect()->to('login');
+    }
+}
